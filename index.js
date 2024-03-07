20c5b5efd79ddc0ff83ae6713a08c93c1a72578e
muskan-fatim <160476952+muskan-fatim@users.noreply.github.com>
2024-03-06 23:13:05 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var message = "Muskan would you like to learn some typscript?";
+console.log(message);
+var person_name = "Muskan";
+console.log(person_name);
+var num = 89;
+var bool = 7 != 7;
+var array = ["muskan", 89, false, true];
+var array2 = ["muskan", "areeba", "hassan"];
+var array3 = [8, 90, 23, 45, 7];
+var array4 = [true, false, true, false];
+var array5 = [89, 90, 78, true, false, true];
---
+++ index.ts
@@ -0,0 +1,12 @@
+
+let message :string = "Muskan would you like to learn some typscript?"
+console.log(message)
+let person_name: string = "Muskan"
+console.log(person_name)
+let num : number = 89;
+let bool : boolean = 7!=7;
+let array : any = ["muskan", 89, false, true]
+let array2 : string [] = ["muskan","areeba","hassan"]
+let array3 : number[] = [8,90,23,45,7]
+let array4 : boolean[] = [true,false,true,false]
+let array5 : (number|boolean)[] = [89,90,78,true, false,true]
